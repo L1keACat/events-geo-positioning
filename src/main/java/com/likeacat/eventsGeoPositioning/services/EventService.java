289d87cf40321110be6c705956e00828eab0ab33
L1keACat <36240624+L1keACat@users.noreply.github.com>
2019-05-08 16:51:50 +0300

    v0.0.2

--- src/main/java/com/likeacat/eventsGeoPositioning/services/EventService.java
+++ src/main/java/com/likeacat/eventsGeoPositioning/services/EventService.java
@@ -3,9 +3,16 @@ package com.likeacat.eventsGeoPositioning.services;
 import com.likeacat.eventsGeoPositioning.DAO.EventDAO;
 import com.likeacat.eventsGeoPositioning.DAO.SequenceDAO;
 import com.likeacat.eventsGeoPositioning.model.Event;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
 
 @Service
@@ -32,5 +39,39 @@ public class EventService {
 
     public void remove(Long id) {
         eventDAO.remove(id);
+    }
+
+    public void random_gen() throws IOException, JSONException {
+        String coord, coord2;
+        String apikey = "e7c0fa9f-f120-48ee-993c-934d78d54607";
+        //double lng = 27.561481;
+        //double lat = 53.902496;
+        double rand_lat, rand_lng;
+        int t;
+        String[] events = {"Выставка", "Велопробег", "Забег", "Хакатон", "Дрифт",
+                "Гонки", "Семинар", "Хакатон", "Концерт", "Авария", "Парад", "Салют"};
+
+        for (int i = 0; i < 10; i++) {
+            rand_lat = 53.84 + (Math.random() * 0.12); //53.84 53.96
+            rand_lng = 27.43 + (Math.random() * 0.24); //27.43 27.67
+            t = (int) (Math.random() * 12);
+
+            coord = rand_lng + "," + rand_lat;
+            coord2 = rand_lat + "," + rand_lng;
+
+            String requestUrl = "https://geocode-maps.yandex.ru/1.x/?format=json&apikey=" + apikey + "&geocode=" + coord;
+
+            JSONObject json = new JSONObject(IOUtils.toString(new URL(requestUrl), Charset.forName("UTF-8")));
+            JSONObject response = (JSONObject) json.get("response");
+            JSONObject GeoObjectCollection = (JSONObject) response.get("GeoObjectCollection");
+            JSONArray featureMember = (JSONArray) GeoObjectCollection.get("featureMember");
+            JSONObject member = featureMember.getJSONObject(0);
+            JSONObject GeoObject = (JSONObject) member.get("GeoObject");
+            String address = (String) GeoObject.get("name");
+
+            Event event = new Event(events[t], address, coord2);
+            event.setId(sequenceDao.getNextSequenceId(Event.COLLECTION_NAME));
+            eventDAO.save(event);
+        }
     }
 }
--- src/main/java/com/likeacat/eventsGeoPositioning/web/MainController.java
+++ src/main/java/com/likeacat/eventsGeoPositioning/web/MainController.java
@@ -2,22 +2,14 @@ package com.likeacat.eventsGeoPositioning.web;
 
 import com.likeacat.eventsGeoPositioning.model.Event;
 import com.likeacat.eventsGeoPositioning.services.EventService;
-import org.apache.commons.io.IOUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 @Controller
 public class MainController {
@@ -66,7 +58,6 @@ public class MainController {
     public String showEditForm(Model model, @RequestParam(required = true) Long id) {
 
         model.addAttribute("event", eventService.get(id));
-        //return new ModelAndView("add_form", "event", eventService.get(id));
 
         return "add_form";
     }
@@ -82,36 +73,7 @@ public class MainController {
     @RequestMapping(value = "/random_gen", method = RequestMethod.GET)
     public String generateRandomEvents() throws IOException, JSONException {
 
-        String coord, coord2;
-        String apikey = "e7c0fa9f-f120-48ee-993c-934d78d54607";
-        //double lng = 27.561481;
-        //double lat = 53.902496;
-        double rand_lat, rand_lng;
-        int t;
-        String[] events = {"Выставка", "Велопробег", "Забег", "Хакатон", "Дрифт",
-                "Гонки", "Семинар", "Хакатон", "Концерт", "Авария", "Парад", "Салют"};
-
-        for (int i = 0; i < 10; i++) {
-            rand_lat = 53.84 + (Math.random() * 0.12); //53.84 53.96
-            rand_lng = 27.43 + (Math.random() * 0.24); //27.43 27.67
-            t = (int) (Math.random() * 12); //27.43 27.67
-
-            coord = rand_lng + "," + rand_lat;
-            coord2 = rand_lat + "," + rand_lng;
-
-            String requestUrl = "https://geocode-maps.yandex.ru/1.x/?format=json&apikey=" + apikey + "&geocode=" + coord;
-
-            JSONObject json = new JSONObject(IOUtils.toString(new URL(requestUrl), Charset.forName("UTF-8")));
-            JSONObject response = (JSONObject) json.get("response");
-            JSONObject GeoObjectCollection = (JSONObject) response.get("GeoObjectCollection");
-            JSONArray featureMember = (JSONArray) GeoObjectCollection.get("featureMember");
-            JSONObject member = featureMember.getJSONObject(0);
-            JSONObject GeoObject = (JSONObject) member.get("GeoObject");
-            String address = (String) GeoObject.get("name");
-
-            Event event = new Event(events[t], address, coord2);
-            eventService.add(event);
-        }
+        eventService.random_gen();
 
         return "redirect:/all";
     }
